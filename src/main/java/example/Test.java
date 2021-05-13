1a0e22790597cd3de09f993f97bf89c103c37a17
Devil <z_m_y007911@126.com>
2021-05-13 23:51:27 +0800

    create

---
+++ src/main/java/example/Test.java
@@ -0,0 +1,8 @@
+package example;
+
+public class Test {
+    public static void main(String[] args) {
+        Http http = new Http();
+        http.mainHandler("13304091772","0020020Ed",66667);
+    }
+}
